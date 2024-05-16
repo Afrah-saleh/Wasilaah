908a93a9adf839a5c68e0f10bd728357d1bd88fa
Afrah-saleh <afrahsaleh49@hotmail.com>
2024-05-16 11:18:06 +0300

    Initial Commit

---
+++ Wasilaah/WasilaahApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasilaahApp.swift
+//  Wasilaah
+//
+//  Created by Afrah Saleh on 08/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WasilaahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
